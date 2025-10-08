384e61b5ded2ca477fa95b07b4037ad763871600
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-10-08 12:09:56 -0300

    update: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,14 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "LearningGuitar 🎸 | Aprenda Violão de Forma Interativa",
-  description: "Aprenda violão online com truques práticos, exercícios interativos e dicas para memorizar acordes, notas e escalas. Ideal para iniciantes e estudantes de música.",
+  description:
+    "Aprenda violão online com truques práticos, exercícios interativos e dicas para memorizar acordes, notas e escalas. Ideal para iniciantes e estudantes de música.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-gradient-to-br from-amber-900 via-yellow-900 to-amber-800 text-white">
         <Header />
         <main className="flex-grow pt-20">{children}</main>
         <Footer />
