f8a3c21d293bd5914d58384e08d58edeb5da418a
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-10-14 23:21:42 -0300

    update: atualizar rota batidas-basicas

--- src/app/batidas-basicas/page.tsx
+++ src/app/batidas-basicas/page.tsx
@@ -6,14 +6,15 @@ import { FaArrowDown, FaArrowUp, FaTimes } from "react-icons/fa";
 const batidas = [
     { nome: "Batida 1", padrao: ["down", "up", "mute"], descricao: "Batida de reggae." },
     { nome: "Batida 2", padrao: ["down", "down", "up", "up", "down"], descricao: "Batida de rock." },
-    { nome: "Batida 3", padrao: ["down", "down", "up", "up", "down", "up"] },
-    { nome: "Batida 4", padrao: ["down", "down", "up", "up", "down", "down"], descricao: "Ideal para louvores suaves e lentos." },
-    { nome: "Batida 5", padrao: ["down", "up", "up"], descricao: "Batida de sertanejo." },
-    { nome: "Batida 6", padrao: ["down", "up", "up", "down"] },
-    { nome: "Batida 7", padrao: ["down", "down", "up", "down", "up", "down"] },
-    { nome: "Batida 8", padrao: ["down", "up", "up", "down", "up", "up", "down"], descricao: "Perfeita para músicas mais animadas." },
-    { nome: "Batida 9", padrao: ["down", "down", "up", "down", "up", "down", "down"], descricao: "Muito usada em músicas pop e adoração." },
-    { nome: "Batida 10", padrao: ["down", "down", "up", "up", "down", "down", "down", "up"] },
+    { nome: "Batida 3", padrao: ["down", "mute", "up", "up", "mute", "up"] },
+    { nome: "Batida 4", padrao: ["down", "down", "up", "up", "down", "up"] },
+    { nome: "Batida 5", padrao: ["down", "down", "up", "up", "down", "down"], descricao: "Ideal para louvores suaves e lentos." },
+    { nome: "Batida 6", padrao: ["down", "up", "up"], descricao: "Batida de sertanejo." },
+    { nome: "Batida 7", padrao: ["down", "up", "up", "down"] },
+    { nome: "Batida 8", padrao: ["down", "down", "up", "down", "up", "down"] },
+    { nome: "Batida 9", padrao: ["down", "up", "up", "down", "up", "up", "down"], descricao: "Perfeita para músicas mais animadas." },
+    { nome: "Batida 10", padrao: ["down", "down", "up", "down", "up", "down", "down"], descricao: "Muito usada em músicas pop e adoração." },
+    { nome: "Batida 11", padrao: ["down", "down", "up", "up", "down", "down", "down", "up"] },
 ];
 
 export default function Batidas() {
