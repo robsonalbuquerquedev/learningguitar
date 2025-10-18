251f67663371b9061f93bdf729f25b02821098b5
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-10-18 00:06:55 -0300

    update: atualizar rota batidas-basicas

--- src/app/batidas-basicas/page.tsx
+++ src/app/batidas-basicas/page.tsx
@@ -11,13 +11,15 @@ const batidas = [
     { nome: "Batida 5", padrao: ["down", "down", "up", "up", "down", "down"], descricao: "Ideal para louvores suaves e lentos." },
     { nome: "Batida 6", padrao: ["down", "down", "up", "up", "down", "down", "up", "down", "up"] },
     { nome: "Batida 7", padrao: ["down", "mute", "up", "up", "down", "mute", "up", "down", "up"] },
-    { nome: "Batida 8", padrao: ["down", "up", "up"], descricao: "Batida de sertanejo." },
-    { nome: "Batida 9", padrao: ["down", "up", "up", "down"] },
-    { nome: "Batida 10", padrao: ["down", "down", "up", "down", "up", "down"] },
-    { nome: "Batida 11", padrao: ["down", "up", "up", "down", "up", "up", "down"], descricao: "Perfeita para músicas mais animadas." },
-    { nome: "Batida 12", padrao: ["down", "down", "up", "down", "up", "down", "down"], descricao: "Muito usada em músicas pop e adoração." },
-    { nome: "Batida 13", padrao: ["down", "down", "up", "up", "down", "down", "down", "up"] },
-    { nome: "Batida 14", padrao: ["down", "up", "up", "down", "up", "up", "down", "up", "down", "up"] },
+    { nome: "Batida 8", padrao: ["down", "down", "up"] },
+    { nome: "Batida 9", padrao: ["down", "mute", "up", "mute", "up"] },
+    { nome: "Batida 10", padrao: ["down", "up", "up"], descricao: "Batida de sertanejo." },
+    { nome: "Batida 11", padrao: ["down", "up", "up", "down"] },
+    { nome: "Batida 12", padrao: ["down", "down", "up", "down", "up", "down"] },
+    { nome: "Batida 13", padrao: ["down", "up", "up", "down", "up", "up", "down"], descricao: "Perfeita para músicas mais animadas." },
+    { nome: "Batida 14", padrao: ["down", "down", "up", "down", "up", "down", "down"], descricao: "Muito usada em músicas pop e adoração." },
+    { nome: "Batida 15", padrao: ["down", "down", "up", "up", "down", "down", "down", "up"] },
+    { nome: "Batida 16", padrao: ["down", "up", "up", "down", "up", "up", "down", "up", "down", "up"] },
 ];
 
 export default function Batidas() {
